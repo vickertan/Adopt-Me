83fe2b3e3de9bd4c08fa1e8e64e48d2871ec8ed4
VickerTan <vickerpangestan@gmail.com>
2023-02-27 16:33:34 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,34 +2,30 @@ const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ])
-}
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Bobo",
-                breed: "Kokoni"
-            }),
-            React.createElement(Pet, {
-                animal: "Bear",
-                name: "Paddington",
-                breed: "Spectacled"
-            }),
-            React.createElement(Pet, {
-                animal: "Hamster",
-                name: "Eve",
-                breed: "Syrian"
-            })
-        ]
-    )
-}
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            animal: "Dog",
+            name: "Bobo",
+            breed: "Kokoni",
+        }),
+        React.createElement(Pet, {
+            animal: "Bear",
+            name: "Paddington",
+            breed: "Spectacled",
+        }),
+        React.createElement(Pet, {
+            animal: "Hamster",
+            name: "Eve",
+            breed: "Syrian",
+        }),
+    ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
