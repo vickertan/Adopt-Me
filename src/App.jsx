9d1ca6f65b44eab9ad607a4bd131e5cff2f7e220
VickerTan <vickerpangestan@gmail.com>
2023-03-01 22:37:20 +0800

    Delete Pet and import SearchParams
    Fix import react-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    <div>
-        <h1>Adopt Me!</h1>
-        <Pet name="Bobo" animal="Dog" breed="Kokoni" />
-        <Pet name="Paddington" animal="Bear" breed="Spectacled" />
-        <Pet name="Eve" animal="Hamster" breed="Dwarf" />
-    </div>;
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+    );
 };
 
 const container = document.getElementById("root");
